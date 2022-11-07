5e9f75de4e88819f73f4994dcc60354bc056884a
Tokio-svg <tokkipt935@gmail.com>
2022-11-07 11:59:26 +0900

    レベル計算処理を追加

--- src/components/MonsterList/MonsterDetail.vue
+++ src/components/MonsterList/MonsterDetail.vue
@@ -2,6 +2,15 @@
   <div>
     <p>{{ MonsterData.name }}</p>
     <radar-chart :chart-data="dataCollection" :chart-options="options" />
+
+    <button @click="changeLevel(-10)">-10</button>
+    <button @click="changeLevel(-1)">-1</button>
+    <div>レベル: {{displayLevel}}</div>
+    <button @click="changeLevel(1)">+1</button>
+    <button @click="changeLevel(10)">+10</button>
+    <div>
+      <button @click="resetLevel">初期レベルにリセット</button>
+    </div>
     <table class="monsterList__table">
       <tr>
         <th>階層</th>
@@ -22,11 +31,11 @@
           <td>{{ MonsterData.floor }}F</td>
           <td>{{ MonsterData.type1 }}</td>
           <td>{{ MonsterData.type2 }}</td>
-          <td class="monsterList__table--number">{{ MonsterData.hp }}</td>
-          <td class="monsterList__table--number">{{ MonsterData.attack }}</td>
-          <td class="monsterList__table--number">{{ MonsterData.deffense }}</td>
+          <td class="monsterList__table--number">{{ displayHP }}</td>
+          <td class="monsterList__table--number">{{ displayATK }}</td>
+          <td class="monsterList__table--number">{{ displayDEF }}</td>
           <td class="monsterList__table--number">{{ MonsterData.exp }}</td>
-          <td class="monsterList__table--number">{{ MonsterData.level }}</td>
+          <td class="monsterList__table--number">{{ displayLevel }}</td>
           <td class="monsterList__table--number">{{ MonsterData.maxLevel }}</td>
           <td class="monsterList__table--number">{{ MonsterData.heal }}</td>
           <td>{{ MonsterData.growth }}</td>
@@ -39,7 +48,18 @@
 
 <script>
 import MonsterData from '@/const/monsterData'
+import PatternData from '@/const/growthPattern'
 import RadarChart from '@/components/Chart/RadarChart.vue'
+
+const patternList = {
+  '普通早熟': 'normalPrecocious',
+  '攻撃早熟': 'attackPrecocious',
+  '防御早熟': 'defensePrecocious',
+  '攻撃晩成': 'attackLateGrowth',
+  '防御晩成': 'defenseLateGrowth',
+  '万能晩成': 'versatileLateGrowth',
+  '守備特殊': 'defenseSpecial'
+}
 
 export default {
   components: {
@@ -65,11 +85,15 @@ export default {
           pointHoverBorderColor: 'rgb(255, 99, 132)'
         }]
       },
-      options: null
+      options: null,
+      displayLevel: 1
     }
   },
 
   mounted() {
+    this.displayLevel = this.MonsterData.level
+
+    // チャート用データ(仮)
     const chartData = {
       labels: [
         'HP',
@@ -127,6 +151,40 @@ export default {
       const id = parseInt(this.$route.params.id)
       const targetData = MonsterData.monsterData.find((v) => v.id === id)
       return targetData
+    },
+
+    growthPattern() {
+      return PatternData[patternList[this.MonsterData.growth]]
+    },
+
+    displayHP() {
+      const param = this.MonsterData.hp + this.growthPattern[this.displayLevel][0] - this.growthPattern[this.MonsterData.level][0]
+      return param
+    },
+
+    displayATK() {
+      const param = this.MonsterData.attack + this.growthPattern[this.displayLevel][1] - this.growthPattern[this.MonsterData.level][1]
+      return param
+    },
+
+    displayDEF() {
+      const param = this.MonsterData.deffense + this.growthPattern[this.displayLevel][2] - this.growthPattern[this.MonsterData.level][2]
+      return param
+    }
+
+  },
+
+  methods: {
+    changeLevel(num) {
+      let level = this.displayLevel
+      level += num
+      if (level < 1) level = 1
+      if (level > this.MonsterData.maxLevel) level = this.MonsterData.maxLevel
+      this.displayLevel = level
+    },
+
+    resetLevel() {
+      this.displayLevel = this.MonsterData.level
     }
   }
 }
