103554ba432c5fe2efd1968aa4a28fdbab7e51a4
Tokio-svg <tokkipt935@gmail.com>
2022-11-09 15:01:06 +0900

    モンスター出現テーブル修正

--- src/components/MonsterTable/MonsterTable.vue
+++ src/components/MonsterTable/MonsterTable.vue
@@ -1,5 +1,97 @@
 <template>
   <div>
     モンスター出現テーブル
+    <table class="monsterTable">
+      <tr>
+        <th>階層</th>
+        <th colspan="10"></th>
+      </tr>
+      <!-- 1~10F -->
+      <tr>
+        <td>1</td>
+        <td rowspan="3">スライム</td>
+        <td rowspan="4">スライムベス</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td rowspan="4">いたずらもぐら</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td rowspan="4">ももんじゃ</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td rowspan="4">おおナメクジ</td>
+        <td rowspan="5">はりせんもぐら</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td rowspan="4">おおきづち</td>
+        <td rowspan="11">オニオ<span class="writing-vertical">ー</span>ン</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td rowspan="5">ホイミスライム</td>
+        <td rowspan="5">ドラキ<span class="writing-vertical">ー</span>Lv2</td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>7</td>
+        <td rowspan="8">ラリホ<span class="writing-vertical">ー</span>アント</td>
+        <td rowspan="5">おばけヒトデ</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>8</td>
+        <td rowspan="5">ゴ<span class="writing-vertical">ー</span>スト</td>
+        <td rowspan="5">タップペンギ<span class="writing-vertical">ー</span></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>9</td>
+        <td rowspan="5">スライムブレス</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr></tr>
+    </table>
   </div>
 </template>
+
+<style scoped>
+@import '@/styles/monsterTable.css';
+</style>
